94dda0cf0c4fd32d85e9394fbe78d947310f1d30
Kapilraj007 <smartkapil18@gmail.com>
2023-08-30 13:14:14 +0530

    deploy
    deploy verscel

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qz&dy)ez$eyx7*#1=_=-c8bbf#(94ctj=so*#)=i%4-48*$+-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =['.vercel.app','now.sh']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR , "static"),
 ]
